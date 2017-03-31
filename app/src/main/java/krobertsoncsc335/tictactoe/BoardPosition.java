4952b2b90539dfa184ca92ed627ea1f7389341c3
Kevron Robertson <kevronr1818@gmail.com>
2017-03-31 12:06:33 -0400

    Added new classes

---
+++ app/src/main/java/krobertsoncsc335/tictactoe/BoardPosition.java
@@ -0,0 +1,10 @@
+package krobertsoncsc335.tictactoe;
+
+/**
+ * Created by Kevron on 3/31/2017.
+ */
+
+public enum BoardPosition {
+
+    PLAYER_ONE,PLAYER_TWO,EMPTY
+}
---
+++ app/src/main/java/krobertsoncsc335/tictactoe/GameBoard.java
@@ -0,0 +1,9 @@
+package krobertsoncsc335.tictactoe;
+
+/**
+ * Created by Kevron on 3/31/2017.
+ */
+
+public class GameBoard {
+
+}
